ee01915ebd35a359cc2b61998293894ad17e2abd
Ronny <ronny761@hityash.org>
2021-11-09 09:09:39 +0200

    sadna 15 - q4

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int education = 0, grading = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter your years of education");
+        education = s.nextInt();
+        System.out.println("enter your total bagrut grade");
+        grading = s.nextInt();
+
+        if ((education >= 15) || (education > 12 && grading > 90)) {
+            System.out.println("congratulation," +
+                    " you have accepted to pilot");
+        }
+        else
+            System.out.println("rejected");
+
+    }
+}
